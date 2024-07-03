897c72b3045d82a1065e475ef6adcf50521c0e2a
Diegocaur <90205488+Diegocaur@users.noreply.github.com>
2024-07-03 02:44:50 -0400

    prueba

---
+++ src/pages/CreateCoffe.js
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import "../styles/loginPage.css";
+
+const CreateCoffee = () => {
+  const [nombre, setNombre] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [precio, setPrecio] = useState("");
+  const [imagen, setImagen] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!nombre || !descripcion || !precio || !imagen) {
+      console.log("Todos los campos son obligatorios");
+      return;
+    }
+
+    const userData = {
+      name: nombre,
+      desc: descripcion,
+      price: precio,
+      foto: imagen,
+    };
+
+    try {
+      const response = await fetch("http://localhost:8081/api/coffee/crear", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Error al registrar café");
+      }
+
+      console.log("Café registrado correctamente");
+
+      // Reiniciar el formulario
+      setNombre("");
+      setDescripcion("");
+      setPrecio("");
+      setImagen(null);
+    } catch (error) {
+      console.error("Error al registrar café:", error);
+    }
+  };
+
+  return (
+    <div>
+      <div className="login">
+        <h1 className="login_titulo">
+          Cafet<span>eria</span> Ma<span>u</span>le
+        </h1>
+        <h1 className="login_titulo">
+          Ingre<span>sa</span> Tu
+          <span>Nuevo</span> Café
+        </h1>
+      </div>
+
+      <form className="login_cuerpo my-10 bg-white shadow rounded-lg p-10">
+        <div className="my-5">
+          <label
+            className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="nombre"
+          >
+            Nombre
+          </label>
+          <input
+            id="nombre"
+            type="text"
+            className="w-full mt-3 p-3 border rounded-xl bg-yellow-50"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+            placeholder="Nombre"
+          />
+
+          <label
+            className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="descripcion"
+          >
+            Descripción
+          </label>
+          <input
+            id="descripcion"
+            type="text"
+            className="w-full mt-3 p-3 border rounded-xl bg-yellow-50"
+            value={descripcion}
+            onChange={(e) => setDescripcion(e.target.value)}
+            placeholder="Descripción"
+          />
+
+          <label
+            className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="precio"
+          >
+            Precio
+          </label>
+          <input
+            id="precio"
+            type="number"
+            className="w-full mt-3 p-3 border rounded-xl bg-yellow-50"
+            value={precio}
+            onChange={(e) => setPrecio(e.target.value)}
+            placeholder="Precio"
+          />
+
+          <label
+            className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="subirImagen"
+          >
+            Subir Imagen
+          </label>
+          <input
+            id="subirImagen"
+            type="file"
+            accept="image/*"
+            className="w-full mt-3 p-3 border rounded-xl bg-yellow-50"
+            onChange={(e) => setImagen(e.target.files[0])}
+          />
+        </div>
+
+        <button
+          className="bg-yellow-800 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-yellow-950 transition-colors"
+          onClick={handleSubmit}
+        >
+          Subir Nuevo Café
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateCoffee;
